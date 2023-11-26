cca5642c250fb9bbfd51e27076814a4107fe55f1
gwolanski <gwol@udel.edu>
2023-11-26 11:50:17 -0700

    Created function that will find all legal moves within the board range for a specified coordinate

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //things to keep in mind to help with efficiency:
 //reduce search space each time the knight traverses closer to its end point. 
 //don't revist nodes/coordinates that we've seen before.
-    //create a variable called that stores all the visited coordinates, perhaps an array?
-    let visitedCoordinates = ["0,0"];
+
 
 //BFS best for finding shortest path from one point to another
 
@@ -11,35 +10,49 @@ class Coordinates {
     constructor([x, y]) {
         this.x = x;
         this.y = y;
+        this.moves = null;
         this.next = null;
     }
 }
 
 class KnightSolution {
     constructor() {
-        this.head = null;
+        this.root = null;
     }
 
+    //find all possible legal moves from specified coordinates that are >= 0 and <= 7
+    findLegalMoves(coordinates, options) {
+        let coordinateX = coordinates[0];
+        let coordinateY = coordinates[1];
 
+        //Minimum and maximum x & y coordinates on board. Coordinates must be >= 0 and <= 7.
+        let xyMin = 0;
+        let xyMax = 7;
+
+        let legalMoves = [];
+
+        //cycles through each move option
+        for (let i = 0; i < options.length; i++) {
+            //for each move option, add coordinateX + first index and coordinateY + second index
+            let currentOption = options[i];
+            let newX = coordinateX + currentOption[0];
+            let newY = coordinateY + currentOption[1];
+            let newMove = [newX, newY];
+            //Check if either new coordinate is between 0 and 7. If true, push to queue.
+            if (newX >= xyMin && newX <= xyMax && newY >= xyMin && newY <= xyMax) {
+                legalMoves.push([newMove]);
+            }
+        }
+        console.log("legalMoves: ", legalMoves);
+        return legalMoves;
+    }
 
     knightMoves(startCoordinates, endCoordinates) {
         let startingCoordinates = new Coordinates(startCoordinates);
         console.log("starting coordinates: ", startingCoordinates)
         let endingCoordinates = new Coordinates(endCoordinates);
         console.log("ending coordinates: ", endingCoordinates);
-        // if (this.head === null) {
-            
-        // }
-        
 
-
-
-
-        //Minimum and maximum x & y coordinates. Coordinates must be >= 0 and <= 7.
-        let xMin = 0;
-        let yMin = 0;
-        let yMax = 7;
-        let xMax = 7;
 
 
 
@@ -59,6 +72,30 @@ class KnightSolution {
 
 
 
+        // while (this.root >= xyMin && this.root <= xyMax) {
+
+        // }
+
+        // find all possible legal moves from the starting point that are >= 0 and <= 7
+        this.findLegalMoves(startCoordinates, moveOptions);
+
+        //declare start of list as being the starting coordinates
+        if (this.root === null) {
+           this.root = startingCoordinates; 
+        }
+        
+        //variable that will store all visited coordinates
+        let visitedCoordinates = [];
+        //variable that will store all coordinate nodes that need to be visited in breadth first order
+        let queue = [this.root];
+
+
+        //BFS logic to search the tree. As node is visited, take it out of queue and place it in visitedCoordinates
+        while (queue.length > 0) {
+            let current = queue.shift();
+            visitedCoordinates.push(current);
+        }
+
         
         //zero moves at start. will need to increase as knight traverses the board
         let moves = 0;
@@ -73,6 +110,9 @@ class KnightSolution {
         //increase moves counter
 
     }
+
+
+
 }
 
 let newSolution = new KnightSolution();
